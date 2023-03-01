de7eb91fd31b19651d7d44fda388974d77c507ec
Aleksandar <aleksandar.vukojevicc@gmail.com>
2023-03-01 21:34:49 +0100

    initial commit

---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,30 @@
+import { useEffect } from 'react';
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
+import classes from './AvailableMeals.module.css';
+
+const AvailableMeals = () => {
+  useEffect(() => {
+    fetch('https://react-http-90428-default-rtdb.europe-west1.firebasedatabase.app/meals.json')
+  },[])
+
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
+};
+
+export default AvailableMeals;
