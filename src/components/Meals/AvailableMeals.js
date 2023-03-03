dea0799dc2ff27977e715a1f3f5c817c7ea1b26e
Aleksandar <aleksandar.vukojevicc@gmail.com>
2023-03-03 18:52:53 +0100

    added the loading animation when fetching meals

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -5,6 +5,7 @@ import classes from './AvailableMeals.module.css';
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     const fetchMeals = async () => {
@@ -23,10 +24,19 @@ const AvailableMeals = () => {
       }
 
       setMeals(loadedMeals)
+      setIsLoading(false)
     }
 
     fetchMeals()
   },[])
+
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    )      
+  }
 
   const mealsList = meals.map((meal) => (
     <MealItem
