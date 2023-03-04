0a1259a8d0271e604e2dfb99a5a22bb6446b8b5f
Aleksandar <aleksandar.vukojevicc@gmail.com>
2023-03-04 19:45:25 +0100

    the error message added when fetching meals from a database fails

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -6,10 +6,16 @@ import classes from './AvailableMeals.module.css';
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([])
   const [isLoading, setIsLoading] = useState(true)
+  const [httpError, setHttpError] = useState()
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch('https://react-http-90428-default-rtdb.europe-west1.firebasedatabase.app/meals.json')
+      
+      if (!response.ok) {
+        throw new Error('Something went wrong!')
+      }
+
       const responseData = await response.json()
 
       const loadedMeals = []
@@ -27,7 +33,10 @@ const AvailableMeals = () => {
       setIsLoading(false)
     }
 
-    fetchMeals()
+    fetchMeals().catch((error) => {
+      setIsLoading(false)
+      setHttpError(error.message)
+    })
   },[])
 
   if (isLoading) {
@@ -36,6 +45,14 @@ const AvailableMeals = () => {
         <p>Loading...</p>
       </section>
     )      
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
+      </section>
+    )
   }
 
   const mealsList = meals.map((meal) => (
